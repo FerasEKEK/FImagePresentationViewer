af28141ba6f82187e95e665c7330082e466f3772
Firas Al Khatib Al Khalidi <fir_khalidi@hotmail.com>
2017-12-09 12:43:47 +0200

    Initial Commit

---
+++ FImagePresentationViewer/FImagePresentationViewer.h
@@ -0,0 +1,19 @@
+//
+//  FImagePresentationViewer.h
+//  FImagePresentationViewer
+//
+//  Created by Firas Al Khatib Al Khalidi on 12/9/17.
+//  Copyright © 2017 Firas Al Khatib Al Khalidi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FImagePresentationViewer.
+FOUNDATION_EXPORT double FImagePresentationViewerVersionNumber;
+
+//! Project version string for FImagePresentationViewer.
+FOUNDATION_EXPORT const unsigned char FImagePresentationViewerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FImagePresentationViewer/PublicHeader.h>
+
+
